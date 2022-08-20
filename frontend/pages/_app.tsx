08967333a2ea4f09774a376c4219f8eed7f4077c
minci <0xminci@gmail.com>
2022-08-20 13:39:22 -0500

    FIXED: stupid git issue

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,92 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Navbar from "../components/Navbar";
+import { useEffect, useState } from "react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  Theme,
+  darkTheme,
+} from "@rainbow-me/rainbowkit";
+import { publicProvider } from "wagmi/providers/public";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import merge from "lodash.merge";
+import { withTheme } from "@emotion/react";
+
+const { chains, provider } = configureChains(
+  [chain.rinkeby, chain.mainnet, chain.polygon, chain.polygonMumbai],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Giftly",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+/* Theming */
+const theme = extendTheme({
+  styles: {
+    global: {
+      // "*": {
+      //   backgroundColor: "transparent",
+      // },
+      a: {
+        _hover: {
+          textDecoration: "underline",
+        },
+      },
+      h1: {
+        fontSize: "4xl",
+        fontWeight: "bold",
+      },
+      h2: {
+        fontSize: "2xl",
+        fontWeight: "bold",
+      },
+      h3: {
+        fontSize: "lg",
+      },
+      h4: {
+        fontSize: "md",
+      },
+    },
+  },
+});
+
+/* RainbowKit Theming */
+const customTheme = merge(darkTheme(), {
+  colors: {
+    accentColor: "#EB498F",
+  },
+} as Theme);
+
+function MyApp({ Component, pageProps, router }: AppProps) {
+  const [mounted, setMounted] = useState(false);
+
+  // prevent hydration UI bug: https://blog.saeloun.com/2021/12/16/hydration.html
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+
+  return (
+    <>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains} theme={customTheme}>
+          <ChakraProvider theme={theme}>
+            <Navbar></Navbar>
+            <Component {...pageProps} key={router.route} />
+          </ChakraProvider>
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </>
+  );
+}
+
+export default MyApp;
